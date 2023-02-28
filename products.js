20d6bc213fdffcd77c5a41bafed1bcac13ec183f
finky2015 <104653759+finky2015@users.noreply.github.com>
2023-02-28 22:14:21 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,83 @@
+const products = [
+  {
+    id: 0,
+    name: "Coffee",
+    price: 4000,
+    instock: 100,
+    description:
+      "Mixture of both flowery, Fruity, herby, nutty, caramelly, chocolaty,Resinous and spicy",
+    imgSrc: "t1.png",
+  },
+  {
+    id: 1,
+    name: "Rice",
+    price: 25000,
+    instock: 43,
+    description:
+      "Rice are unformity of size an shape, whiteness, long and thin uncooked grains..",
+    imgSrc: "t2.png",
+  },
+  {
+    id: 2,
+    name: "Flour",
+    price: 2500,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "t3.png",
+  },
+  {
+    id: 3,
+    name: "Agashya",
+    price: 6000,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "t4.png",
+  },
+  {
+    id: 4,
+    name: "Olive Oil",
+    price: 4500,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "t5.png",
+  },
+  {
+    id: 5,
+    name: "Tissue paper",
+    price: 10000,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "t6.png",
+  },
+  {
+    id: 6,
+    name: "guarantee",
+    price: 4500,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "t6.png",
+  },
+  {
+    id: 7,
+    name: "guarantee",
+    price: 4500,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "t6.png",
+  },
+  {
+    id: 8,
+    name: "guarantee",
+    price: 4500,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "t6.png",
+  },
+];
